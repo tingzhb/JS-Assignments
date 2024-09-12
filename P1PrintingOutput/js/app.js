9a9a723796a9b2079ad891f6a309d4594f1f88b9
Benjamin Ting <tingzhb@gmail.com>
2024-09-12 11:07:49 +0200

    Finish P1 Printing Output

--- P1PrintingOutput/js/app.js
+++ P1PrintingOutput/js/app.js
@@ -1,2 +1,10 @@
-console.log("Hello World!");
-console.log("My name is Bennnnnnnnnn");
+const five = 5;
+const eight = 8;
+let total = 0;
+
+console.log(five);
+console.log(eight);
+// 5 + 8 = 13
+total = five + eight;
+console.log(total);
+console.log(five + eight);
