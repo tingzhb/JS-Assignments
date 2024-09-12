da1b6cf0627c5723ccb4e3d22690413696cd9529
Benjamin Ting <tingzhb@gmail.com>
2024-09-12 15:29:50 +0200

    Finish Mathematics Part 1

--- P5Operators/js/app.js
+++ P5Operators/js/app.js
@@ -3,7 +3,20 @@ let speedOutput = 0;
 speedOutput = speedInput / 3.6;
 console.log(speedOutput);
 
+
 let minutesInput = 3;
 let secondsOutput = 0;
-// ....
+secondsOutput = minutesInput * 60;
 console.log(secondsOutput);
+
+let divisionInput1 = 11;
+let divisionInput2 = 4;
+let divisionOutput = 0;
+divisionOutput = divisionInput1 / divisionInput2;
+console.log(divisionOutput);
+
+let remainderOutput = 0;
+remainderOutput = divisionInput1 % divisionInput2;
+console.log(remainderOutput);
+
+Math.Pi;
