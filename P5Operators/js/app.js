f51e34cb93645d845d3879ca060994cfb8254281
Benjamin Ting <tingzhb@gmail.com>
2024-09-12 14:11:06 +0200

    Add basic Operators

---
+++ P5Operators/js/app.js
@@ -0,0 +1,42 @@
+// let firstNumber = 7;
+// let secondNumber = 2;
+// let answer = 0;
+// // 1 + 2 = 3
+// answer = firstNumber + secondNumber;
+// console.log(answer);
+//
+// answer = firstNumber - secondNumber;
+//
+// console.log(answer);
+//
+// answer = firstNumber * secondNumber;
+// console.log(answer);
+//
+// answer = firstNumber / secondNumber;
+// console.log(answer);
+//
+// answer = firstNumber % secondNumber;
+// console.log(answer);
+
+
+let gold = 50;
+gold + 10;
+console.log(gold); // 50
+
+gold = gold + 10;
+console.log(gold); // 60
+
+gold += 10;
+console.log(gold); // 70
+
+gold *= 10;
+console.log(gold); // 700
+
+gold /= 10;
+console.log(gold); // 70
+
+gold -= 10;
+console.log(gold); // 60
+
+gold %= 2;
+console.log(gold); // 0
