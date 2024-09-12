4bade48029ec91756c7f9c0afa710c9686e5b991
Benjamin Ting <tingzhb@gmail.com>
2024-09-12 14:51:54 +0200

    Add Assignment Structure

---
+++ P5Operators/js/app.js
@@ -0,0 +1,9 @@
+let speedInput = 72;
+let speedOutput = 0;
+speedOutput = speedInput / 3.6;
+console.log(speedOutput);
+
+let minutesInput = 3;
+let secondsOutput = 0;
+// ....
+console.log(secondsOutput);
