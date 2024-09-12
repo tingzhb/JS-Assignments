911cb1e1ecaa2a49c553995ce4793fd8008877fa
Benjamin Ting <tingzhb@gmail.com>
2024-09-12 14:41:46 +0200

    Shift app to app 2

---
+++ P5Operators/js/app2.js
@@ -0,0 +1,63 @@
+// USING 2 VARIABLES AND STORING THEM IN ANSWER
+// let firstNumber = 7;
+// let secondNumber = 2;
+// let answer = 0;
+// // 1 + 2 = 3
+// answer = firstNumber + secondNumber;
+// console.log(answer);
+//
+// answer = firstNumber - secondNumber;
+//
+// console.log(answer);
+//
+// answer = firstNumber * secondNumber;
+// console.log(answer);
+//
+// answer = firstNumber / secondNumber;
+// console.log(answer);
+//
+// answer = firstNumber % secondNumber;
+// console.log(answer);
+
+// MODIFYING A VARIABLE WITH NUMBERS
+// let gold = 50;
+// gold + 10;
+// console.log(gold); // 50
+//
+// gold = gold + 10;
+// console.log(gold); // 60
+//
+// gold += 10;
+// console.log(gold); // 70
+//
+// gold *= 10;
+// console.log(gold); // 700
+//
+// gold /= 10;
+// console.log(gold); // 70
+//
+// gold -= 10;
+// console.log(gold); // 60
+//
+// gold %= 2;
+// console.log(gold); // 0
+
+// PRE/POST INCREMENT (++ X OR X ++)
+let numberOfStudents = 0;
+
+numberOfStudents += 1;
+console.log(numberOfStudents); // 1
+
+numberOfStudents++;
+console.log(numberOfStudents); // 2
+
+++numberOfStudents;
+console.log(numberOfStudents); // 3
+
+console.log(numberOfStudents++); // 3 WHY?
+console.log(numberOfStudents); // 4 WHY???
+
+console.log(++numberOfStudents); // 5
+
+// numberOfStudents--;
+// console.log(numberOfStudents); // 1
