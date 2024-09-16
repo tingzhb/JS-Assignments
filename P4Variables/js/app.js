421ece816ec843a9816ec7c260854d70d9ff3737
Benjamin Ting <tingzhb@gmail.com>
2024-09-16 10:24:25 +0200

    Refactor Green Bottles

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -1,17 +1,6 @@
-// Assign the value 10 to a new let variable named numberOfBottles
-// Assign the value "green bottles hanging on the wall" to a new  const variable named line1
-// Assign the value "and if one green bottle, should accidentally fall, there will be" to a new const variable named line2
-// console.log() each value in sequence to form the Output.
- // P.S. You can deduct from a number variable using --numberOfBottles;
-
 let numberOfBottles = 10;
-const line1 = "green bottles hanging on the wall";
-const line2 = "and if one green bottle, should accidentally fall, there will be";
+const line1 = " green bottles hanging on the wall. ";
+const line2 = "And if one green bottle, should accidentally fall, there will be ";
 
-console.log(numberOfBottles);
-console.log(line1);
-console.log(numberOfBottles);
-console.log(line1);
-console.log(line2);
-console.log(--numberOfBottles);
-console.log(line1);
+console.log(numberOfBottles + line1 + numberOfBottles + line1);
+console.log(line2 + --numberOfBottles + line1);
