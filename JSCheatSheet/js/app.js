a2828a92c094f443af7a498e59c664fa14be32ff
Benjamin Ting <tingzhb@gmail.com>
2024-09-17 09:41:20 +0200

    Add JS Cheatsheet

---
+++ JSCheatSheet/js/app.js
@@ -0,0 +1,63 @@
+// INITIALIZING VARIABLES
+// Variable that CANNOT be modified
+const firstName = "Ben";
+const lastName = "Ting";
+
+// Variable that CAN be modified
+let age = 30;
+let isAwake = true;
+
+// INITIALIZING FUNCTIONS
+// Function with no inputs or outputs
+function sayGoodMorning() {
+  console.log("Good Morning!");
+  console.log(age);
+  age++;
+}
+
+// Function with no inputs and an output
+function getBankBalance() {
+  return 500 + 600;
+}
+
+// Calling
+console.log(getBankBalance());
+
+// Function with input(s) and no output
+function addOne(inputNumber) {
+  inputNumber += 1;
+  console.log(inputNumber)
+}
+
+// Calling
+
+addOne(8);
+
+// Function with input(s) and an output
+function square(numberToSquare) {
+  return numberToSquare * numberToSquare;
+}
+
+// Calling
+let squaredNumber = square(15);
+console.log(squaredNumber);
+
+console.log(square(15));
+
+// RUNNING
+// Modifying Variables
+height = 175;
+isAwake = false;
+let yearOfBirth = 2024 - age;
+// firstName = "Jonathan"; Does not work because firstName is const
+
+// Calling a Function
+sayGoodMorning();
+
+// Using a Bool to control code flow
+if (isAwake) {
+  console.log("Awake!");
+}
+else {
+  console.log("Asleep!")
+}
